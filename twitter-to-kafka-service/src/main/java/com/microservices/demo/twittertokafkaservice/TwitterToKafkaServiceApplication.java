be0c8472ac8b329467a837964fac42f80aac5d5b
xgaslann <gorkem_aslan0@hotmail.com>
2025-06-10 23:37:11 +0300

    Initial commit

---
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twittertokafkaservice/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.demo.twittertokafkaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+}
---
+++ twitter-to-kafka-service/src/test/java/com/microservices/demo/twittertokafkaservice/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.demo.twittertokafkaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterToKafkaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
