e67866a6843947f96a70e2ca93d86fe2f8674406
xgaslann <gorkem_aslan0@hotmail.com>
2025-06-11 00:11:16 +0300

    Add Twitter stream processing functionality to collect tweets and log status
    - Introduced `StreamRunner` interface and its implementation `TwitterKafkaStreamRunner`.
    - Configured Twitter access credentials and keywords in `application.yml` and `twitter4j.properties`.
    - Implemented `TwitterKafkaStatusListener` to handle tweet events.
    - Updated `TwitterToKafkaServiceApplication` to start the stream on application launch.
    - Added dependency for `twitter4j-stream`.

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twittertokafkaservice/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twittertokafkaservice/TwitterToKafkaServiceApplication.java
@@ -1,13 +1,36 @@
 package com.microservices.demo.twittertokafkaservice;
 
+import com.microservices.demo.twittertokafkaservice.config.TwitterToKafkaServiceConfigData;
+import com.microservices.demo.twittertokafkaservice.runner.StreamRunner;
+import lombok.extern.java.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwitterToKafkaServiceApplication {
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+
+    private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+    private final StreamRunner streamRunner;
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData, StreamRunner streamRunner) {
+        this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
+        this.streamRunner = streamRunner;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("Twitter to Kafka Service Application has started successfully.");
+        LOG.info("Twitter Keywords: {}", twitterToKafkaServiceConfigData.getTwitterKeywords());
+        streamRunner.start();
+    }
 }
---
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twittertokafkaservice/runner/impl/TwitterKafkaStreamRunner.java
@@ -0,0 +1,48 @@
+package com.microservices.demo.twittertokafkaservice.runner.impl;
+
+import com.microservices.demo.twittertokafkaservice.config.TwitterToKafkaServiceConfigData;
+import com.microservices.demo.twittertokafkaservice.listener.TwitterKafkaStatusListener;
+import com.microservices.demo.twittertokafkaservice.runner.StreamRunner;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import twitter4j.FilterQuery;
+import twitter4j.TwitterException;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+
+@Slf4j
+@Component
+public class TwitterKafkaStreamRunner implements StreamRunner {
+    private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+    private final TwitterKafkaStatusListener twitterKafkaStatusListener;
+
+    private TwitterStream twitterStream;
+
+    public TwitterKafkaStreamRunner(TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData, TwitterKafkaStatusListener twitterKafkaStatusListener) {
+        this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
+        this.twitterKafkaStatusListener = twitterKafkaStatusListener;
+    }
+
+    @PreDestroy
+    public void shutdown() {
+        if (twitterStream != null) {
+            twitterStream.shutdown();
+            log.info("Twitter stream has been shut down.");
+        } else {
+            log.warn("Twitter stream was not running.");
+        }
+    }
+
+    @Override
+    public void start() throws TwitterException {
+        twitterStream = new TwitterStreamFactory().getInstance();
+        twitterStream.addListener(twitterKafkaStatusListener);
+        addFilter();
+    }
+
+    private void addFilter() {
+        twitterStream.filter(new FilterQuery().track(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[0])));
+        log.info("Started Twitter stream with keywords: {}", twitterToKafkaServiceConfigData.getTwitterKeywords());
+    }
+}
