af56c5cb2d410256e757bed5d533742502353e54
xgaslann <gorkem_aslan0@hotmail.com>
2025-06-15 00:49:48 +0300

    Add Kafka modules: `kafka-model`, `kafka-admin`, and `kafka-producer` with initial setup
    - Created three new modules: `kafka-model`, `kafka-admin`, and `kafka-producer`.
    - Added Maven configurations, Spring Boot setup, and entry-point classes for each module.
    - Included test files to verify application context loading.
    - Added Maven wrapper scripts and `.gitignore` files for each module.

---
+++ kafka/kafka-model/src/test/java/com/microservices/demo/kafka/model/KafkaModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.demo.kafka.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
